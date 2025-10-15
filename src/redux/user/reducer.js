4e8674cb22a513bb8e7a3f88c05e5e5c8214b985
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2025-10-14 22:11:56 -0300

    feat: add user reducer

---
+++ src/redux/user/reducer.js
@@ -0,0 +1,19 @@
+import { act } from 'react'
+import UserActionTypes from './action-types'
+
+const initialState = {
+  currentUser: null,
+}
+
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case UserActionTypes.LOGIN:
+      return { ...state, currentUser: action.payload }
+    case UserActionTypes.LOGOUT:
+      return { ...state, currentUser: null }
+    default:
+      return state
+  }
+}
+
+export default userReducer
